adc0cf4b0eda39ad1d3e41ecc04c3f91e1abb192
ruan.azevedo <ruannilton@gmail.com>
2023-08-24 19:27:54 -0300

    entry

--- client/main.c
+++ client/main.c
@@ -1,15 +1,12 @@
-#include <core/caffeine_logging.h>
-#include <core/caffeine_memory.h>
+#include <caffeine_entry.h>
 
-int main(int argc, char **argv) {
-  cff_memory_init();
-  caff_log_init();
+int caff_main(int argc, char **argv) {
+
   caff_log_error("ERROR\n");
   caff_log_warn("Warning\n");
   caff_log_trace("Tracing\n");
   caff_log_info("Hello %s\n", "World");
   caff_log_debug("Hoy Hey\n");
-  caff_log_end();
-  cff_memory_end();
+  
   return 0;
 }
